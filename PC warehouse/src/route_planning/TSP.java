57dd536e22fafc851cccd9ce9510490f07c0a2ad
Csongor Barabasi <barabasi.csongi96@gmail.com>
2017-03-20 19:15:50 +0000

    Improved smart job assignment, added permutation handling for choosing the best permutation for assigning the routes to the robots

--- PC warehouse/src/route_planning/TSP.java
+++ PC warehouse/src/route_planning/TSP.java
@@ -12,7 +12,7 @@ public class TSP {
   private int mapSizeX, mapSizeY;
   private RoutePlanner planner;
   private Travel travel;
-  
+
   private final double startingTemperature = 0.7;
   private final int numberOfIterations = 200;
   private final double coolingRate = 2.5;
@@ -25,13 +25,13 @@ public class TSP {
   }
 
   public void simulateAnnealing(ArrayList<Location> locations) {
-    
+
 	if (locations.size() <= 1){
 		return;
 	}
-	  
+
 	travel = new Travel(locations, planner);
-    
+
     System.out.println(locations.size());
 
     System.out.println("Starting SA with temperature: " + startingTemperature + ", # of iterations: " + numberOfIterations + " and colling rate: " + coolingRate);
@@ -67,21 +67,79 @@ public class TSP {
     locations.clear();
     locations.addAll(bestSolution.getTravel());
   }
-  
+
   public HashMap<String, ArrayList<Location>> smartAssignment(Location r1S, Location r2S, Location r3S, ArrayList<Location> route1, ArrayList<Location> route2, ArrayList<Location> route3) {
 	  ArrayList<Location> sol1 = new ArrayList<Location>();
 	  ArrayList<Location> sol2 = new ArrayList<Location>();
 	  ArrayList<Location> sol3 = new ArrayList<Location>();
-	  ArrayList<Location> temp1, temp2, temp3;
-	  
+	  ArrayList<Location> temp1 = new ArrayList<Location>();
+    ArrayList<Location> temp2 = new ArrayList<Location>();
+    ArrayList<Location> temp3 = new ArrayList<Location>();
+
 	  // 1/2/3
-	  temp1 = new ArrayList<>(); temp1.add(r1S); temp1.addAll(route1); simulateAnnealing(temp1);
-	  temp2 = new ArrayList<>(); temp2.add(r2S); temp1.addAll(route2); simulateAnnealing(temp2);
-	  temp3 = new ArrayList<>(); temp3.add(r3S); temp1.addAll(route3); simulateAnnealing(temp3);
-	  
-	  // To be continued
-	  
+	  temp1.clear(); temp1.add(r1S); temp1.addAll(route1); simulateAnnealing(temp1);
+	  temp2.clear(); temp2.add(r2S); temp2.addAll(route2); simulateAnnealing(temp2);
+	  temp3.clear(); temp3.add(r3S); temp3.addAll(route3); simulateAnnealing(temp3);
+    if(travel.getDistance(temp1) < travel.getDistance(sol1) && travel.getDistance(temp2) < travel.getDistance(sol2) && travel.getDistance(temp3) < travel.getDistance(sol3)) {
+      sol1.clear(); sol1.addAll(temp1);
+      sol2.clear(); sol1.addAll(temp2);
+      sol3.clear(); sol1.addAll(temp3);
+    }
+
+    // 1/3/2
+    temp1.clear(); temp1.add(r1S); temp1.addAll(route1); simulateAnnealing(temp1);
+    temp2.clear(); temp2.add(r2S); temp2.addAll(route3); simulateAnnealing(temp2);
+    temp3.clear(); temp3.add(r3S); temp3.addAll(route2); simulateAnnealing(temp3);
+    if(travel.getDistance(temp1) < travel.getDistance(sol1) && travel.getDistance(temp2) < travel.getDistance(sol2) && travel.getDistance(temp3) < travel.getDistance(sol3)) {
+      sol1.clear(); sol1.addAll(temp1);
+      sol2.clear(); sol1.addAll(temp2);
+      sol3.clear(); sol1.addAll(temp3);
+    }
+
+    // 2/1/3
+	  temp1.clear(); temp1.add(r1S); temp1.addAll(route2); simulateAnnealing(temp1);
+	  temp2.clear(); temp2.add(r2S); temp2.addAll(route1); simulateAnnealing(temp2);
+	  temp3.clear(); temp3.add(r3S); temp3.addAll(route3); simulateAnnealing(temp3);
+    if(travel.getDistance(temp1) < travel.getDistance(sol1) && travel.getDistance(temp2) < travel.getDistance(sol2) && travel.getDistance(temp3) < travel.getDistance(sol3)) {
+      sol1.clear(); sol1.addAll(temp1);
+      sol2.clear(); sol1.addAll(temp2);
+      sol3.clear(); sol1.addAll(temp3);
+    }
+
+    // 2/3/1
+	  temp1.clear(); temp1.add(r1S); temp1.addAll(route2); simulateAnnealing(temp1);
+	  temp2.clear(); temp2.add(r2S); temp2.addAll(route3); simulateAnnealing(temp2);
+	  temp3.clear(); temp3.add(r3S); temp3.addAll(route1); simulateAnnealing(temp3);
+    if(travel.getDistance(temp1) < travel.getDistance(sol1) && travel.getDistance(temp2) < travel.getDistance(sol2) && travel.getDistance(temp3) < travel.getDistance(sol3)) {
+      sol1.clear(); sol1.addAll(temp1);
+      sol2.clear(); sol1.addAll(temp2);
+      sol3.clear(); sol1.addAll(temp3);
+    }
+
+    // 3/1/2
+	  temp1.clear(); temp1.add(r1S); temp1.addAll(route3); simulateAnnealing(temp1);
+	  temp2.clear(); temp2.add(r2S); temp2.addAll(route1); simulateAnnealing(temp2);
+	  temp3.clear(); temp3.add(r3S); temp3.addAll(route2); simulateAnnealing(temp3);
+    if(travel.getDistance(temp1) < travel.getDistance(sol1) && travel.getDistance(temp2) < travel.getDistance(sol2) && travel.getDistance(temp3) < travel.getDistance(sol3)) {
+      sol1.clear(); sol1.addAll(temp1);
+      sol2.clear(); sol1.addAll(temp2);
+      sol3.clear(); sol1.addAll(temp3);
+    }
+
+    // 3/2/1
+	  temp1.clear(); temp1.add(r1S); temp1.addAll(route3); simulateAnnealing(temp1);
+	  temp2.clear(); temp2.add(r2S); temp2.addAll(route2); simulateAnnealing(temp2);
+	  temp3.clear(); temp3.add(r3S); temp3.addAll(route1); simulateAnnealing(temp3);
+    if(travel.getDistance(temp1) < travel.getDistance(sol1) && travel.getDistance(temp2) < travel.getDistance(sol2) && travel.getDistance(temp3) < travel.getDistance(sol3)) {
+      sol1.clear(); sol1.addAll(temp1);
+      sol2.clear(); sol1.addAll(temp2);
+      sol3.clear(); sol1.addAll(temp3);
+    }
+
 	  HashMap<String, ArrayList<Location>> result = new HashMap<String, ArrayList<Location>>();
+    result.put("robot1", sol1);
+    result.put("robot2", sol2);
+    result.put("robot3", sol3);
 	  return result;
   }
 
